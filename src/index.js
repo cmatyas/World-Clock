815c5ea5530ac52ed727476e33c7063f56ed6612
cmatyas <119801952+cmatyas@users.noreply.github.com>
2025-07-28 15:19:30 -0400

    Added current location and updates each second

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-setInterval(function() {
-}, 1000);
-
 function updateCity (event) {
-    let cityTimeZone = event.target.value;
-    let cityTime = moment.tz(cityTimeZone);
-    let citiesEleemnt = document.querySelector("#cities");
-    citiesEleemnt.innerHTML = `
-    <div class="city">
-        <div>
-            <h2>${cityTimeZone.replace("_", " ").split("/")[1]}</h2>
-            <div class="date">${cityTime.format("MMMM DD YYYY")}</div>
-        </div>
-        <div class="time">${cityTime.format("hh:mm:ss [<small>]A[</small>]")}</div>
-    </div>
-    `;
+    let citiesElement = document.querySelector("#cities");
+        citiesElement.innerHTML = "";
+    setInterval(function() {
+        let cityTimeZone = event.target.value;
+        if (cityTimeZone === "current") {
+            cityTimeZone = moment.tz.guess();
+        }
+            let cityTime = moment.tz(cityTimeZone);
+            let citiesElement = document.querySelector("#cities");
+            citiesElement.innerHTML = `
+            <div class="city">
+                <div>
+                    <h2>${cityTimeZone.replace("_", " ").split("/")[1]}</h2>
+                    <div class="date">${cityTime.format("MMMM DD YYYY")}</div>
+                </div>
+                <div class="time">${cityTime.format("hh:mm:ss [<small>]A[</small>]")}</div>
+            </div>
+            `;
+    }, 1000);
 }
 
 let citiesSelectElement = document.querySelector("#city");
