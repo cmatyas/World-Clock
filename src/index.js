168fa14e21fe429bbb09b31153b58b20996cc73a
cmatyas <119801952+cmatyas@users.noreply.github.com>
2025-07-28 16:47:19 -0400

    Added link to all cities

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+setInterval(function () {
+    let londonElement = document.querySelector("#london");
+    let londonDate = londonElement.querySelector(".date")
+    let londonTime = londonElement.querySelector(".time")
+    londonDate.innerHTML = moment().format("MMMM DD YYYY");
+    londonTime.innerHTML = moment().tz("Europe/London").format("hh:mm:ss [<small>]A[</small>]");
+}, 1000);
+
+setInterval(function () {
+        let losAngelesElement = document.querySelector("#losAngeles");
+    let losAngelesDate = losAngelesElement.querySelector(".date")
+    let losAngelesTime = losAngelesElement.querySelector(".time")
+    losAngelesDate.innerHTML = moment().format("MMMM DD YYYY");
+    losAngelesTime.innerHTML = moment().tz("America/Los_Angeles").format("hh:mm:ss [<small>]A[</small>]");
+}, 1000);
+
+setInterval(function() {
+    let philadelphiaElement = document.querySelector("#philadelphia");
+    let philaDate = philadelphiaElement.querySelector(".date");
+    let philaTime = philadelphiaElement.querySelector(".time");
+    philaDate.innerHTML = moment().format("MMMM DD YYYY");
+    philaTime.innerHTML = moment().tz("America/New_York").format("hh:mm:ss [<small>]A[</small>]");
+}, 1000);
+
 function updateCity (event) {
     let citiesElement = document.querySelector("#cities");
         citiesElement.innerHTML = "";
@@ -16,8 +40,10 @@ function updateCity (event) {
                 </div>
                 <div class="time">${cityTime.format("hh:mm:ss [<small>]A[</small>]")}</div>
             </div>
+            <a href="/">Back to All Cities</a>
             `;
     }, 1000);
+
 }
 
 let citiesSelectElement = document.querySelector("#city");
